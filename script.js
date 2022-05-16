56e12fdf093d1d4618f21d8d7bd07bcfce1c977e
Pooyan <thepooyan@gmail.com>
2022-05-16 15:52:48 +0430

    all onclicks removed and made into script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function openResMenu() {
-    document.getElementById("resMenu").style.width = "320px";
-  }
+document.getElementById('resOpen').addEventListener('click', function() {
+  document.getElementById("resMenu").style.width = "320px";
+})
+document.getElementById('resClose').addEventListener('click', function() {
+  document.getElementById("resMenu").style.width = "0";
+})
   
-  function closeResMenu() {
-    document.getElementById("resMenu").style.width = "0";
-  }
 
 var coll = document.getElementsByClassName("collapsible");
 for (let i = 0; i < coll.length; i++) {
@@ -23,15 +23,22 @@ let courseL1 = document.getElementsByClassName("courseSelector");
 
 for (let i = 0; i < courseL1.length; i++) {
   courseL1[i].addEventListener("click", function() {
-    document.getElementById(this.dataset.linkto).classList.toggle('noDisplay');
+    document.getElementById(this.dataset.linkto).classList.remove('noDisplay');
     document.getElementsByClassName("OriginContent")[0].classList.add("out");
     document.getElementsByClassName("crossContent")[0].classList.add("in");
   });
 }
 
-function navigateBackToRes() {
+document.getElementById('backToRes').addEventListener('click', function() {
   let courseL2 = document.querySelector('.crossContent ul:not(.noDisplay)');
   courseL2.classList.add('noDisplay');
   document.getElementsByClassName("OriginContent")[0].classList.remove("out");
   document.getElementsByClassName("crossContent")[0].classList.remove("in");
+})
+
+let clicker = document.querySelectorAll('[data-onClick]');
+for (let i=0; i < clicker.length; i++) {
+  clicker[i].addEventListener('click', function() {
+    clicker[i].classList.toggle(clicker[i].getAttribute('data-onClick'));
+  })
 }
